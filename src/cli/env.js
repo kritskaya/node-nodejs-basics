e4bacdea9a1ad71d0becc8f131b3b2b8046ebc2d
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-12-05 13:19:42 +0300

    feat: implement cli/env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
+import { env } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+  const vars = Object.entries(env)
+    .filter((entry) => entry[0].startsWith('RSS_'))
+    .reduce((acc, entry) => acc + `; ${entry[0]}=${entry[1]}`, '')
+    .slice(1);
+
+  console.log(vars);
 };
 
 parseEnv();
