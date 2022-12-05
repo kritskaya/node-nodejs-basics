428c677b2655428898fda07bd9d5094c337c4df4
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2022-12-05 20:08:32 +0300

    feat: implement cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { spawn } from 'node:child_process';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const FILENAME = 'script.js';
+  const FOLDER = 'files';
+
+  const filePath = join(
+    dirname(fileURLToPath(import.meta.url)),
+    FOLDER,
+    FILENAME
+  );
+  spawn('node', [filePath, ...args], { stdio: [0, 1, 'ipc'] });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['a', 'b', 'c']);
