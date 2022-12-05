97413e6b5743c34c53ba1c5fdea4b593cef9e468
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-12-05 13:35:33 +0300

    feat: implement streams/read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import { createReadStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { stdin, stdout } from 'node:process';
+import { fileURLToPath } from 'node:url';
+
 const read = async () => {
-    // Write your code here 
+  const FILENAME = 'fileToRead.txt';
+  const FOLDER = 'files';
+
+  const filePath = join(
+    dirname(fileURLToPath(import.meta.url)),
+    FOLDER,
+    FILENAME
+  );
+
+  const stream = createReadStream(filePath);
+
+  let fileContent = '';
+  stream.on('data', (chunk) => (fileContent += chunk));
+  stream.on('end', () => stdout.write(fileContent));
 };
 
 await read();
