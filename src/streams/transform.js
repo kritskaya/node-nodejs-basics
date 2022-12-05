2ab7a14960f03a3e851afda31e65ffbb8817183a
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-12-05 13:37:18 +0300

    feat: implement streams/transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,27 @@
+import { stdin, stdout } from 'node:process';
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+  const stream = new Transform({
+    transform(chunk, encoding, callback) {
+      const content = chunk.toString();
+
+      let transformedContent = '';
+      if (content.endsWith('\n')) {
+        transformedContent = content
+          .slice(0, content.length - 1)
+          .split('')
+          .reverse()
+          .join('');
+      }
+      callback(null, `${transformedContent}\n`);
+    },
+  });
+
+  stdout.write('Input your data\n');
+  stdout.write('Press Ctrl+C for exit\n');
+  stdin.pipe(stream).pipe(stdout);
 };
 
 await transform();
