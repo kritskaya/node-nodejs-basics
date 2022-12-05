a1d2339d419e3bfa0aaa9aa038e824e38655ed4d
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-12-05 13:36:16 +0300

    feat: implement streams/write task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import { createWriteStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { stdin, stdout } from 'node:process';
+import { fileURLToPath } from 'node:url';
+
 const write = async () => {
-    // Write your code here 
+  const FILENAME = 'fileToWrite.txt';
+  const FOLDER = 'files';
+
+  const filePath = join(
+    dirname(fileURLToPath(import.meta.url)),
+    FOLDER,
+    FILENAME
+  );
+
+  const stream = createWriteStream(filePath);
+  stdout.write('Input your data\n');
+  stdout.write('Press Ctrl+C for exit\n');
+  stdin.on('data', (data) => stream.write(data));
 };
 
 await write();
