1fd13f5e0dad83eef56442cb9a1aa8651908ae0b
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-12-05 13:33:42 +0300

    feat: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from 'node:crypto';
+import { readFile } from 'node:fs/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const FILENAME = 'fileToCalculateHashFor.txt';
+  const FOLDER = 'files';
+  const filePath = path.join(
+    dirname(fileURLToPath(import.meta.url)),
+    FOLDER,
+    FILENAME
+  );
+
+  const content = await readFile(filePath);
+
+  const hash = createHash('sha256').update(content).digest('hex');
+  console.log(hash);
 };
 
 await calculateHash();
