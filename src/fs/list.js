d9c4e2cb111e52f19deec95fbd7fb2914fb77035
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-12-05 13:14:50 +0300

    feat: implement fs/list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,27 @@
+import { constants } from 'node:fs';
+import { access, readdir } from 'node:fs/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const list = async () => {
-    // Write your code here 
+  const FOLDER_NAME = 'files';
+
+  const folderPath = path.join(
+    dirname(fileURLToPath(import.meta.url)),
+    FOLDER_NAME
+  );
+
+  try {
+    await access(folderPath, constants.F_OK);
+  } catch {
+    throw new Error('FS operation failed');
+  }
+
+  const files = await readdir(folderPath);
+
+  for (const file of files) {
+    console.log(file);
+  }
 };
 
 await list();
