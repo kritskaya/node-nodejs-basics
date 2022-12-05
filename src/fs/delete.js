95c9ddb2a3205abf8e160cf9489ffd9c19eea015
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-12-05 13:13:32 +0300

    feat: implement fs/delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+import { rm } from 'node:fs/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const remove = async () => {
-    // Write your code here 
+  const FOLDER_NAME = 'files';
+  const FILENAME = 'fileToRemove.txt';
+
+  const filePath = path.join(
+    dirname(fileURLToPath(import.meta.url)),
+    FOLDER_NAME,
+    FILENAME
+  );
+
+  try {
+    await rm(filePath);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
