8f01d10299c0fef94c89e6a0845fd45c295c193f
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2022-12-05 19:00:06 +0300

    feat: implement worker thread task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,31 +1,39 @@
 import { cpus } from 'node:os';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { Worker } from 'node:worker_threads';
 
 const performCalculations = async () => {
   const FILENAME = 'worker.js';
 
-  const filePath = join(
-    dirname(fileURLToPath(import.meta.url)),
-    FILENAME
-  );
+  const filePath = join(dirname(fileURLToPath(import.meta.url)), FILENAME);
 
   const coresNumber = cpus().length;
 
   const workers = [];
-  const counter = 10;
+  let counter = 10;
 
-  for(let i = 0; i < coresNumber; i++) {
+  for (let i = 0; i < coresNumber; i++) {
     const worker = new Worker(filePath);
     worker.postMessage(counter++);
     workers.push(worker);
   }
 
-  const results = await Promise.allSettled(workers.map((worker) => {
-    new Promise(() => {
-      worker.on('message', (value) => {
+  const results = await Promise.allSettled(
+    workers.map(
+      (worker) =>
+        new Promise((resolve, reject) => {
+          worker.on('message', (value) => {
+            resolve({ status: 'resolved', data: value });
+          });
+          worker.on('error', (value) => {
+            resolve({ status: 'error', data: null });
+          });
+        })
+    )
+  );
 
-      });
-    })
-  }))
+  results.forEach((result) => console.log(result.value));
 };
 
 await performCalculations();
