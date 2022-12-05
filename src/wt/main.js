d3d275251b2fb884418493b5f0b208fd2214e766
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-12-05 16:49:51 +0300

    feat: start worker thread task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import { cpus } from 'node:os';
+
 const performCalculations = async () => {
-    // Write your code here
+  const FILENAME = 'worker.js';
+
+  const filePath = join(
+    dirname(fileURLToPath(import.meta.url)),
+    FILENAME
+  );
+
+  const coresNumber = cpus().length;
+
+  const workers = [];
+  const counter = 10;
+
+  for(let i = 0; i < coresNumber; i++) {
+    const worker = new Worker(filePath);
+    worker.postMessage(counter++);
+    workers.push(worker);
+  }
+
+  const results = await Promise.allSettled(workers.map((worker) => {
+    new Promise(() => {
+      worker.on('message', (value) => {
+
+      });
+    })
+  }))
 };
 
 await performCalculations();
